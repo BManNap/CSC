86608e979e8e7308b532200242fd1adc19e9dcc2
Brandon <bnn5011@gmail.com>
2024-10-10 22:41:39 -0500

    File setup and Basics of home page sorted out. Needs a lot of css to look good...... Good luck!

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require('chunks/menu.html');?>
+    <h1>The Cloquet Service Center</h1>
+    <p><?php require('chunks/home.txt');?></p>
+</body>
+</html>
