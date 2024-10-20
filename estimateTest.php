0d48b7d9a92cbfdcecd68117999f27fb29af2095
Brandon <bnn5011@gmail.com>
2024-10-19 20:40:54 -0500

    BS
    Made a test file using bootstrap.
    I like the look of it better.
    Start converting the nav bar to a bootstrap nav bar.

---
+++ estimateTest.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BootStrap Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-light">
+        <div class="container-xxl">
+            <a class="navbar-brand" href="index.php">Cloquet Service Center</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="estimate.php">Estimate</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+                </ul>
+</div>
+            
+        </div>
+    </nav>
+    <nav class="menu">
+        <ul class="row">
+            <li class="col"><a href="index.php">Home</a></li>
+            <li class="col"><a href="estimate.php">Estimate</a></li>
+            <li class="col"><a href="about.html">About</a></li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
