dae237f6f7af10eb324efdddf6bf1d3e7bdaed2a
Brandon <bnn5011@gmail.com>
2024-10-19 20:45:01 -0500

    BS Navbar
    Updated the menu chunk to include BS.
    Made CSS into a chunk to allow easy edit of CSS links for all pages.

--- estimate.php
+++ estimate.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estimate</title>
-    <link rel="stylesheet" href="css/style.css">
+    <?php require('chunks/cssLinks.html');?>
+    
 </head>
 <body>
     <?php require('chunks/menu.html');?>
--- estimateTest.php
+++ estimateTest.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BootStrap Test</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php require('chunks/menu.html');?>
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-light">
@@ -17,7 +17,7 @@
                     <li class="nav-item"><a class="nav-link" href="estimate.php">Estimate</a></li>
                     <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
                 </ul>
-</div>
+            </div>
             
         </div>
     </nav>
