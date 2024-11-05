c3abfb03a20cd72457e83d54ec87f36d0d4f1d97
Brandon <bnn5011@gmail.com>
2024-11-05 00:05:06 -0600

    Setting up database
    Looking at how I'm gonna set up the database/

--- estimate.php
+++ estimate.php
@@ -8,22 +8,25 @@
     <?php require('chunks/jsLinks.html');?>
 </head>
 <body>
-    <?php require('chunks/menu.html');?>
-    <h1>Make an Estimate</h1>
-    <hr>
-    <form class="estimateForm" method="post" action="/total.php">
-        <input type="checkbox" id="oilChange" name="oilChange" value="oilChange"/>
-        <label for="oilChange">Oil Change</label>
-        <input type="checkbox" id="rotate" name="alignment" value="alignment"/>
-        <label for="alignment">Alignment</label>
-        <input type="checkbox" id="bulb" name="bulb" value="bulb"/>
-        <label for="bulb">Bulb Replacement</label>
-        <input type="checkbox" id="tireLeak" name="tireLeak" value="tireLeak"/>
-        <label for="tireLeak">Tire Leak Repair</label>
-        <input type="checkbox" id="tireNew" name="tireNew" value="tireNew"/>
-        <label for="tireNew">New Tires</label>
+    <?php require('chunks/menu.html');
+    echo '
+        <h1>Make an Estimate</h1>
+        <hr>
+        <form class="estimateForm" method="post" action="/total.php">
+            <input type="checkbox" id="oilChange" name="oilChange" value="oilChange"/>
+            <label for="oilChange">Oil Change</label>
+            <input type="checkbox" id="rotate" name="alignment" value="alignment"/>
+            <label for="alignment">Alignment</label>
+            <input type="checkbox" id="bulb" name="bulb" value="bulb"/>
+            <label for="bulb">Bulb Replacement</label>
+            <input type="checkbox" id="tireLeak" name="tireLeak" value="tireLeak"/>
+            <label for="tireLeak">Tire Leak Repair</label>
+            <input type="checkbox" id="tireNew" name="tireNew" value="tireNew"/>
+            <label for="tireNew">New Tires</label>
 
-        <input type="submit" value="Submit"/>
-    </form>
+            <input type="submit" value="Submit"/>
+        </form>
+        '
+    ?>
 </body>
 </html>
